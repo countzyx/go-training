62729108f3cdf7bc9161512e7f7642455c5e08a0
Pedro R. Jiménez <countzyx@gmail.com>
2013-09-13 16:48:19 -0400

    Completed Exercise 70 - Equivalent Binary Trees
    I decided to create 4 trees, two seeded with 1 and two seeded with 2.
    Those with the same seeds should be the same and no combination of
    mismatched seeds should ever be the same. The results prove this.

--- tour.golang.org/70.go
+++ tour.golang.org/70.go
@@ -9,34 +9,56 @@ import (
 // Walk walks the tree t sending all values
 // from the tree to the channel ch.
 func Walk(t *tree.Tree, ch chan int) {
-	WalkSubtree(t, ch)
+	walkSubtree(t, ch)
 
 	close(ch)
 }
 
-func WalkSubtree(t *tree.Tree, ch chan int) {
+func walkSubtree(t *tree.Tree, ch chan int) {
 	if t.Left != nil {
-		WalkSubtree(t.Left, ch)
+		walkSubtree(t.Left, ch)
 	}
 	ch <- t.Value
 
 	if t.Right != nil {
-		WalkSubtree(t.Right, ch)
+		walkSubtree(t.Right, ch)
 	}
 }
 
 // Same determines whether the trees
 // t1 and t2 contain the same values.
-//func Same(t1, t2 *tree.Tree) bool
+func Same(t1, t2 *tree.Tree) bool {
+	bIsSame := true
+
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+
+	for result1 := range ch1 {
+		result2 := <-ch2
+		bIsSame = result1 == result2
+		if !bIsSame {
+			break
+		}
+	}
+
+	return bIsSame
+}
 
 func main() {
-	ch := make(chan int)
+	tree1_1 := tree.New(1)
+	tree1_2 := tree.New(1)
+	tree2_1 := tree.New(2)
+	tree2_2 := tree.New(2)
 
-	go Walk(tree.New(1), ch)
-
-	for result := range ch {
-		fmt.Println(result)
-	}
+	fmt.Printf("Tree1_1 == Tree1_2? %t\n", Same(tree1_1, tree1_2))
+	fmt.Printf("Tree2_1 == Tree2_2? %t\n", Same(tree2_1, tree2_2))
+	fmt.Printf("Tree1_1 == Tree2_1? %t\n", Same(tree1_1, tree2_1))
+	fmt.Printf("Tree1_1 == Tree2_2? %t\n", Same(tree1_1, tree2_2))
+	fmt.Printf("Tree1_2 == Tree2_1? %t\n", Same(tree1_2, tree2_1))
+	fmt.Printf("Tree1_2 == Tree2_2? %t\n", Same(tree1_2, tree2_2))
 
 	os.Exit(0)
 }
